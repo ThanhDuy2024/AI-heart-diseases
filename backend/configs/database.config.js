2f70246f46437a24608393b7f548f6d0bdb34362
Lam Thanh Duy <thanhduythptthanhbinh@gmail.com>
2026-06-04 09:30:38 +0700

    [Update] connect database

--- backend/configs/database.config.js
+++ backend/configs/database.config.js
@@ -1,6 +1,46 @@
-const sequelize = require("sequelize");
+const { Sequelize } = require('sequelize');;
 
-sequelize = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+const sequelize = new Sequelize(
+  process.env.DATABASE_AI_HEART_DISEASES,
+  process.env.DATABASE_APP_USER,
+  process.env.DATABASE_APP_PASSWORD,
+  {
+    host: "database",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
+
+const connectDatabase = async (
+  retries = 10,
+  delay = 5000
+) => {
+  for (let i = 1; i <= retries; i++) {
+    try {
+      await sequelize.authenticate();
+      console.log("Connection has been established successfully.");
+      return;
+    } catch (error) {
+      console.error(
+        `Connect fail lần ${i}/${retries}`
+      );
+
+      if (i === retries) {
+        console.error("Hết số lần reconnect.");
+        console.error(error);
+        process.exit(1);
+      }
+
+      console.log(`Thử lại sau ${delay / 1000}s...`);
+
+      await new Promise((resolve) =>
+        setTimeout(resolve, delay)
+      );
+    }
+  }
+};
+
+module.exports = {
+    connectDatabase,
+    sequelize
+}
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+require('dotenv').config();
 const cookieParser = require("cookie-parser");
 const indexRoute = require("./routes/index.route.js");
+const connectDatabase = require("./configs/database.config.js");
 const app = express();
 const port = 4000;
-
+connectDatabase.connectDatabase();
 app.use(express.json());
 app.use(cookieParser());
 
